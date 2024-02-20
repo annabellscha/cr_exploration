f3262ff539d0a08ce8c852a3897bae162e67b350
Annabell Schaefer <123618421+annabellscha@users.noreply.github.com>
2024-02-20 14:51:00 +0100

    feat retry

--- cr_extraction/main.py
+++ cr_extraction/main.py
@@ -43,7 +43,7 @@ def search_companies_by_id(request):
         return "Bad Request: Please provide a company name", 400
 
     retriever = CommercialRegisterRetriever()
-    companies = retriever.extended_search(name)
+    companies = retriever.search(name)
 
     return jsonify(companies)
 
