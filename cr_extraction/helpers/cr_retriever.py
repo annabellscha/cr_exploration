b0aefca9167f8cb558cddaf9081e6cc1a5dde048
Annabell Schaefer <123618421+annabellscha@users.noreply.github.com>
2024-02-26 18:36:58 +0100

    switch error logging

--- cr_extraction/helpers/cr_retriever.py
+++ cr_extraction/helpers/cr_retriever.py
@@ -53,11 +53,12 @@ class CommercialRegisterRetriever:
         while True:
             elements = self.browser.page.select("div.dktree-container.level-{} span a".format(level))
             if len(elements) == 0:
-                document_manager._write_error_to_db("no gs list found", company_id)
+                
                 raise Exception("no gs list found")
             if level == 3:
                 element = list(filter(lambda x: x.text == "Liste der Gesellschafter", elements))
                 if len(element) == 0:
+                    document_manager._write_error_to_db("no gs list found", company_id)
                     raise Exception("no gs list found")
                 self.browser.open_relative(element[0].attrs["href"])
                 level += 1
