859cf8aeabb66c94c5ccaaaf0abe5c6b3a716497
Annabell Schaefer <123618421+annabellscha@users.noreply.github.com>
2024-02-28 12:40:24 +0100

    fix/ cr

--- cr_extraction/helpers/cr_retriever.py
+++ cr_extraction/helpers/cr_retriever.py
@@ -63,7 +63,7 @@ class CommercialRegisterRetriever:
                 if len(element) == 0:
                     document_manager._write_error_to_db("no gs list found", company_id)
                     raise Exception("no gs list found")
-                self.browser.open_relative(element[0].attrs["href"])
+                self.browser.open_relative(element[1].attrs["href"])
                 level += 1
                 continue
             if "Liste der" not in elements[0].text:
