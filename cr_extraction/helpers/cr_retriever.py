160f3c165b445b8cd66b3ae3a832a956e9ea3f52
Annabell Schaefer <123618421+annabellscha@users.noreply.github.com>
2024-03-04 15:00:54 +0100

    fix

--- cr_extraction/helpers/cr_retriever.py
+++ cr_extraction/helpers/cr_retriever.py
@@ -95,10 +95,10 @@ class CommercialRegisterRetriever:
                     # # Find the element with 2021 or earlier as date
                     # #drop dates that are not 2021 or earlier
                     #take subset of dates_elements that are 2021 or earlier
-                    dates_elements = [x for x in dates_elements if x[1].year <= 2021]
+                    dates_elements = [x for x in dates_elements if x[0].year <= 2021]
                     
                     # dates_elements = [x for x in dates_elements if x[0].year <= 2021]
-                    element = max(dates_elements, key=lambda x: x[0])[0] if dates_elements else None
+                    element = max(dates_elements, key=lambda x: x[0])[1] if dates_elements else None
                     print(elements[1].text)
                     self.browser.open_relative(element.attrs["href"])
                     print(f"Elements in if statement 3: {elements[1]}")
